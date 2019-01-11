2fe93418126dd02c4fb7d9437e422426a26d923f
Adam Fahrer <17009995@saultcollege.ca>
2019-01-10 19:37:53 -0500

    lab1a-2 hello world

---
+++ lab1a-2/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class HelloWorld
+    {
+        static public string hello
+        {
+            get
+            {
+                return "Hello";
+            }
+        }
+        static public string world()
+        {
+            return "World";
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HelloWorld.hello + " " + HelloWorld.world() + "!");
+            Console.ReadKey();
+        }
+
+    }
+}
