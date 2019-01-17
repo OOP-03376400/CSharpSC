64492176c16304cde5fa25bce45302a835e73cde
Adam Fahrer <17009995@saultcollege.ca>
2019-01-17 18:33:07 -0500

    started qu2

---
+++ Lab2a/Qu2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qu2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectInt = new Rectangle<int>(3, 5);
+            var rectDouble = new Rectangle<double>(3.25, 4.25);
+           
+        }
+    }
+}
---
+++ Lab2a/Qu2/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace Qu2
+{
+    public class Rectangle<T>
+    {
+        private T length;
+        private T width;
+
+        public Rectangle(T l, T w)
+        {
+            length = l;
+            width = w;
+        }
+        public double area()
+        {
+            return length * width;
+        }
+    }
+}
