9caca9625dd36fe9d7b19b2b8ca26a38ad97e3a8
Adam Fahrer <17009995@saultcollege.ca>
2019-01-17 12:31:09 -0500

    HelloWorld class is in separate file. created an array of locations instead of using separate variables

---
+++ lab1a-2/ConsoleApp/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp
+{
+    class HelloWorld
+    {
+        static public string hello
+        {
+            get
+            {
+                return "Hello";
+            }
+        }
+        static public string getLocation(Location l)
+        {
+            switch (l)
+            {
+                case Location.World:
+                    return "World";
+                case Location.Universe:
+                    return "Universe";
+                case Location.Galaxy:
+                    return "Galaxy";
+                default:
+                    return "Nobody";
+            }
+        }
+    }
+}
--- lab1a-2/ConsoleApp/Program.cs
+++ lab1a-2/ConsoleApp/Program.cs
@@ -8,42 +8,23 @@ namespace ConsoleApp
         Universe,
         Galaxy
     }
-    class HelloWorld
-    {
-        static public string hello
-        {
-            get
-            {
-                return "Hello";
-            }
-        }
-        static public string getLocation(Location l)
-        {
-            switch (l)
-            {
-                case Location.World:
-                    return "World";
-                case Location.Universe:
-                    return "Universe";
-                case Location.Galaxy:
-                    return "Galaxy";
-                default:
-                    return "Nobody";
-            }
-        }
-    }
     class Program
     {
 
         static void Main(string[] args)
         {
-            Location w = Location.World;
-            Location g = Location.Galaxy;
-            Location u = Location.Universe;
-            Console.WriteLine(HelloWorld.hello + " " + HelloWorld.getLocation(w) + "!" +
-                "\n" + HelloWorld.hello + " " + HelloWorld.getLocation(g) + "!" +
-                "\n" + HelloWorld.hello + " " + HelloWorld.getLocation(u) + "!");
-            /* Output
+            /* 
+             * Each index in the locate array stores a different value so that the 
+             * corresponding string value is returned when the getLocation function is called
+             */
+            Location[] locate = { Location.World, Location.Galaxy, Location.Universe };
+
+            foreach(Location item in locate)
+            {
+                Console.WriteLine(HelloWorld.hello + " " + HelloWorld.getLocation(item) + "!");
+            }
+            /* 
+             * Output
              * Hello World!
              * Hello Galaxy!
              * Hello Universe!
