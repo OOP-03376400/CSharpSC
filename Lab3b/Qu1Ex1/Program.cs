c4fa8a6c88bf046d8db58af19da054596c5c2bcf
Adam Fahrer <17009995@saultcollege.ca>
2019-01-24 19:44:07 -0500

    started lab3b

---
+++ Lab3b/Qu1Ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qu1Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using the Ilist interface to instantiate list objects using different types
+            IList<int> intList = new List<int>();
+            //Add method is adding elements to the list
+            intList.Add(10);
+            intList.Add(20);
+            intList.Add(30);
+            intList.Add(40);
+
+            IList<string> strList = new List<string>();
+            strList.Add("one");
+            strList.Add("two");
+            strList.Add("three");
+            strList.Add("four");
+            strList.Add("four");
+            strList.Add(null);
+            strList.Add(null);
+
+            IList<Student> studentList = new List<Student>();
+            studentList.Add(new Student());
+            studentList.Add(new Student());
+            studentList.Add(new Student());
+        }
+    }
+}
