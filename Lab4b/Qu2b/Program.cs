b3ca7a011a98a2e633847cdcd93dc604f62f231b
Adam Fahrer <17009995@saultcollege.ca>
2019-02-14 19:10:53 -0500

    Added Comments to lab4b

--- Lab4b/Qu2b/Program.cs
+++ Lab4b/Qu2b/Program.cs
@@ -6,33 +6,36 @@ using System.Threading.Tasks;
 
 namespace Qu2b
 {
+    //Program is the subsriber class
     class Program
     {
         static void Main(string[] args)
         {
+            //pass random value to the Counter Constructor
             Counter c = new Counter(new Random().Next(10));
             c.ThresholdReached += c_ThresholdReached;
 
             Console.WriteLine("press 'a' key to increase total");
+            //waits until a key is pressed
             while (Console.ReadKey(true).KeyChar == 'a')
             {
                 Console.WriteLine("adding one");
                 c.Add(1);
             }
         }
-
+        //event handler ThesholdReached Event Data to the console
         static void c_ThresholdReached(object sender, ThresholdReachedEventArgs e)
         {
             Console.WriteLine("The threshold of {0} was reached at {1}.", e.Threshold, e.TimeReached);
             Environment.Exit(0);
         }
     }
-
+    //Adder is the publisher class
     class Counter
     {
         private int threshold;
         private int total;
-
+        //threshold takes on value of random number
         public Counter(int passedThreshold)
         {
             threshold = passedThreshold;
@@ -41,8 +44,10 @@ namespace Qu2b
         public void Add(int x)
         {
             total += x;
+            // trigger the event when a's pressed is >= the threshold value
             if (total >= threshold)
             {
+                //insert values into event Arguments
                 ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
                 args.Threshold = threshold;
                 args.TimeReached = DateTime.Now;
@@ -52,6 +57,7 @@ namespace Qu2b
 
         protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
         {
+            //invoke the delagate
             EventHandler<ThresholdReachedEventArgs> handler = ThresholdReached;
             if (handler != null)
             {
@@ -61,7 +67,7 @@ namespace Qu2b
 
         public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
     }
-
+    //Contains properties that are used as event data
     public class ThresholdReachedEventArgs : EventArgs
     {
         public int Threshold { get; set; }
