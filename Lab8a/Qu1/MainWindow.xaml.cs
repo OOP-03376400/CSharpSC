a73071adff7e2d1cc8fcec85cca5cd6013a378a3
Adam Fahrer <17009995@saultcollege.ca>
2019-03-18 17:23:40 -0400

    Qu1 Complete

---
+++ Lab8a/Qu1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Qu1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString =
+                "Data Source=.\\SQLEXPRESS01;Initial Catalog=ProductDB;"
+                + "Integrated Security=true";
+
+            // Provide the query string with a parameter placeholder.
+            string queryString =
+                "SELECT * FROM dbo.Products";
+
+            // Create and open the connection in a using block. This
+            // ensures that all resources will be closed and disposed
+            // when the code exits.
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+
+                // Open the connection in a try/catch block. 
+                // Create and execute the DataReader, writing the result
+                // set to the console window.
+                connection.Open();
+                using (var DTProducts = new DataTable())
+                {
+                    DTProducts.Load(command.ExecuteReader());
+                    dataList.ItemsSource = DTProducts.DefaultView;
+                }
+                connection.Close();
+                   
+            }
+        }
+    }
+}
