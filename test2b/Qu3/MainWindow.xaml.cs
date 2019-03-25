36ec8132718e6e3c0df62580ab184e6725a6363e
Adam Fahrer <17009995@saultcollege.ca>
2019-03-25 18:19:23 -0400

    Qu3 and qu4 complete

---
+++ test2b/Qu3/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Qu3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString =
+                "Data Source=.\\SQLEXPRESS01;Initial Catalog=ProductDB;"
+                + "Integrated Security=true";
+
+            string queryString =
+                "SELECT * FROM dbo.Products";
+
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                using (var DTProducts = new DataTable())
+                {
+                    DTProducts.Load(command.ExecuteReader());
+                    dataList.ItemsSource = DTProducts.DefaultView;
+                }
+                connection.Close();
+            }
+        }
+    }
+}
