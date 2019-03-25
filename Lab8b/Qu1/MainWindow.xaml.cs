363fc9be1d3ef136b72eec936fc522a66d81b870
Adam Fahrer <17009995@saultcollege.ca>
2019-03-25 13:13:55 -0400

    Qu1 Complete

---
+++ Lab8b/Qu1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Qu1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            using (var DB = new ProductDBEntities())
+            {
+                var LProducts = (from P in DB.Products
+                                 orderby P.UnitPrice ascending
+                                 select new { P.ProductID, P.UnitPrice, P.ProductName }).ToList();
+                dataList.ItemsSource = LProducts;
+
+
+            }
+            
+        }
+    }
+}
